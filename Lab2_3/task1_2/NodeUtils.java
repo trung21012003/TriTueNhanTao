317ee72857f94ffc7e0f7ccfc8c9303e3b68e7ba
tranquoctrung <21130590@st.hcmuaf.edu.vn>
2023-10-30 21:07:58 +0700

    lab_4

---
+++ Lab2_3/task1_2/NodeUtils.java
@@ -0,0 +1,24 @@
+package task1_2;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class NodeUtils {
+	public static List<String> printPath(Node node) {
+		if (node != null) {
+			List<String> result = new ArrayList<>();
+			result.add(node.getLabel());
+			Node tmp;
+			while ((tmp = node.getParent()) != null) {
+				result.add(tmp.getLabel());
+				node = tmp;
+			}
+			Collections.reverse(result);
+			return result;
+		} else
+			return new ArrayList<>(Arrays.asList("Khong ton tai duong di"));
+	}
+}
---
+++ Lab_4_AI/src/AI/IInformedSearchAlgo.java
@@ -0,0 +1,8 @@
+package AI;
+
+public interface IInformedSearchAlgo {
+	public Node execute(Node root, String goal);
+
+	public Node execute(Node root, String start, String goal);
+
+}
